b44970371148a43ef1861898dd82ff43fa469941
rahulgitacc <37959182+rahulgitacc@users.noreply.github.com>
2018-03-31 21:15:09 +0530

    Example with and without using linq method

---
+++ Part1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Part1
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var filePath = @"C:\Users\rahul\Desktop\Pictures";
+            TreditionalMethod(filePath);
+            UseingLinqExpression(filePath);
+            Console.Read();
+        }
+
+        /// <summary>
+        /// UseingLinqExpression example
+        /// </summary>
+        /// <param name="filePath">File Path</param>
+        private static void UseingLinqExpression(string filePath)
+        {
+            var filesList = new DirectoryInfo(filePath).GetFiles().OrderByDescending(x => x.Name).Take(10);
+            foreach (var fileInfo in filesList)
+                Console.WriteLine(fileInfo.Name);
+        }
+
+        /// <summary>
+        /// TreditionalMethod example
+        /// </summary>
+        /// <param name="filePath">File Path</param>
+        private static void TreditionalMethod(string filePath)
+        {
+            var directoryInfo = new DirectoryInfo(filePath);
+            var getFiles = directoryInfo.GetFiles();
+            var count = 0;
+            foreach (var files in getFiles)
+            {
+                count++;
+                //By ascending order
+                Console.WriteLine(files.Name);
+            }
+
+            // By descending order
+            for (var i = count - 1; i >= 0; i--)
+                Console.WriteLine(getFiles[i].Name);
+
+            Console.WriteLine($"Total number of files is {count}");
+        }
+    }
+}
